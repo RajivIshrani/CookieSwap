b91d13f98857fe97dda8eae70c5a01a786fb3b31
Ishrani Rajiv <rajivishrani@gmail.com>
2022-10-27 16:16:29 +0530

    deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,28 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat")
+require("dotenv").config({ path: ".env" })
+const { COOKIE_TOKEN_CONTRACT_ADDRESS } = require("../constants")
 
-async function main() {}
+async function main() {
+    const cookieTokenAddress = COOKIE_TOKEN_CONTRACT_ADDRESS
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    const exchangeContract = await ethers.getContractFactory("Exchange")
+
+    // here we deploy the contract
+    const deployedExchangeContract = await exchangeContract.deploy(
+        cookieTokenAddress
+    )
+    await deployedExchangeContract.deployed()
+    console.log("Instance Of Contract", deployedExchangeContract)
+    console.log("-------------------------------------------------------------")
+
+    // print the address of the deployed contract
+    console.log("Exchange Contract Address:", deployedExchangeContract.address)
+}
+
+// Call the main function and catch if there is any error
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
